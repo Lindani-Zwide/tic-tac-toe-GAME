59360d7afe973324d713cd1c7a9a0c8d25220889
Lindani-Zwide <222034990@stu.ukzn.ac.za>
2025-01-30 07:44:14 +0200

    Added more DRY(yer) code in js file and more

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const dropdownContent = document.querySelector('.dropdown-content');
 
 // Get players
 const playersElement = document.getElementById('players');
+playersElement.innerHTML = `/------------------------------------------/`;
 
 // Get winner announcement
 const winnerAnnouncement = document.querySelector('.winner-announcement');
@@ -15,6 +16,7 @@ const winnerText = document.getElementById('winner-text');
 
 // Get reset game button
 const resetGameButton = document.getElementById('reset-game');
+resetGameButton.style.pointerEvents='none'
 
 // Game variables
 let currentPlayer = 'X';
@@ -52,8 +54,11 @@ function handleCellClick(event) {
   }
 }
 
-// Function to check win
-function checkWin() {
+// Function to check win and winning cells
+function checkWin_and_winningCells() {
+  let win=false;
+  let winningCels=[];
+  let win_and_cells=[];
   const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -70,36 +75,25 @@ function checkWin() {
       cells[condition[0]].innerHTML === cells[condition[1]].innerHTML &&
       cells[condition[0]].innerHTML === cells[condition[2]].innerHTML
     ) {
-      return true;
+      winningCells=condition;
+      win_and_cells=[winningCells,true];
+      return win_and_cells;
     }
   }
-  return false;
+  
+  return win_and_cells;
 }
 
-// Function to get winning cells
-function getWinningCells() {
-  const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (const condition of winConditions) {
-    if (
-      cells[condition[0]].innerHTML !== '' &&
-      cells[condition[0]].innerHTML === cells[condition[1]].innerHTML &&
-      cells[condition[0]].innerHTML === cells[condition[2]].innerHTML
-    ) {
-      return condition;
-    }
-  }
-  return [];
+//function to check win
+function checkWin(){
+  const is_Win=checkWin_and_winningCells()[1];
+  return is_Win;
 }
-
+//function to get winning cells
+function getWinningCells(){
+  const winningCells=checkWin_and_winningCells()[0];
+  return winningCells;
+}
 // Function to highlight winning cells
 function highlightWinningCells() {
   winningCells.forEach((index) => {
@@ -143,44 +137,44 @@ function enableCells() {
 
 // Function to handle computer move
 function computerMove() {
-const emptyCells = Array.from(cells).filter((cell) => cell.innerHTML === '');
-const bestMove = getBestMove(emptyCells);
-bestMove.innerHTML = `<img src="Assets/O.png" alt="O-icon" height="50" width="50">`;
-if (checkWin()) {
-winningCells = getWinningCells();
-highlightWinningCells();
-updateScores();
-winnerText.textContent = 'Computer wins!';
-winnerAnnouncement.style.display = 'flex';
-gameOver = true;
-disableCells();
-} else if (checkDraw()) {
-winnerText.textContent = `It's a draw!`;
-winnerAnnouncement.style.display = 'flex';
-gameOver = true;
-disableCells();
-} else {
-currentPlayer = 'X';
-}
+  const emptyCells = Array.from(cells).filter((cell) => cell.innerHTML === '');
+  const bestMove = getBestMove(emptyCells);
+  bestMove.innerHTML = `<img src="Assets/O.png" alt="O-icon" height="50" width="50">`;
+  if (checkWin()) {
+    winningCells = getWinningCells();
+    highlightWinningCells();
+    updateScores();
+    winnerText.textContent = 'Computer wins!';
+    winnerAnnouncement.style.display = 'flex';
+    gameOver = true;
+    disableCells();
+  } else if (checkDraw()) {
+    winnerText.textContent = `It's a draw!`;
+    winnerAnnouncement.style.display = 'flex';
+    gameOver = true;
+    disableCells();
+  } else {
+    currentPlayer = 'X';
+  }
 }
 
 // Function to get best move
 function getBestMove(emptyCells) {
-const corners = emptyCells.filter((cell) => {
-const index = Array.from(cells).indexOf(cell);
-return [0, 2, 6, 8].includes(index);
-});
-if (corners.length > 0) {
-return corners[0];
-}
-const center = emptyCells.find((cell) => {
-const index = Array.from(cells).indexOf(cell);
-return index === 4;
-});
-if (center) {
-return center;
-}
-return emptyCells[0];
+  const corners = emptyCells.filter((cell) => {
+  const index = Array.from(cells).indexOf(cell);
+  return [0, 2, 6, 8].includes(index);
+  });
+  if (corners.length > 0) {
+    return corners[0];
+  }
+  const center = emptyCells.find((cell) => {
+  const index = Array.from(cells).indexOf(cell);
+  return index === 4;
+  });
+  if (center) {
+   return center;
+  }
+  return emptyCells[0];
 }
 
 // Add event listeners to cells
@@ -193,43 +187,44 @@ dropdownButton.addEventListener('click', toggleDropDown);
 
 // Add event listeners to game mode options
 document.querySelectorAll('.option').forEach((option) => {
-option.addEventListener('click', () => {
-gameMode = option.id;
-playersElement.innerHTML = `<span>${your_score}</span>-You VS ${gameMode}-<span>${opponent_score}</span>`;
-dropdownContent.classList.toggle('show');
-toggleDropDown();
-resetGame();
-});
-});
+  option.addEventListener('click', () => {
+  gameMode = option.id;
+  document.getElementById('reset-game').style.pointerEvents='auto'
+  playersElement.innerHTML = `<span>${your_score}</span>-You VS ${gameMode}-<span>${opponent_score}</span>`;
+  dropdownContent.classList.toggle('show');
+  toggleDropDown();
+  resetGame();
+  });
+  });
 
 // Add event listener to reset game button
 resetGameButton.addEventListener('click', resetGame);
 
 // Function to reset game
 function resetGame() {
-dropdownButton.innerHTML=`SELECT OPPONENT  &#8592`;
-your_score = 0;
-opponent_score = 0;
-playersElement.innerHTML = `<span>${your_score}</span>-You VS ${gameMode}-<span>${opponent_score}</span>`;
-cells.forEach((cell) => {
-cell.innerHTML = '';
-cell.style.backgroundColor = '';
-});
-winnerAnnouncement.style.display = 'none';
-gameOver = false;
-currentPlayer = 'X';
-enableCells();
-dropdownContent.classList.toggle('show');
+   dropdownButton.innerHTML=`SELECT OPPONENT  &#8592`;
+   your_score = 0;
+   opponent_score = 0;
+   playersElement.innerHTML = `<span>${your_score}</span>-You VS ${gameMode}-<span>${opponent_score}</span>`;
+   cells.forEach((cell) => {
+   cell.innerHTML = '';
+   cell.style.backgroundColor = '';
+   });
+   winnerAnnouncement.style.display = 'none';
+   gameOver = false;
+   currentPlayer = 'X';
+   enableCells();
+   dropdownContent.classList.toggle('show');
 }
 
 // Function to toggle dropdown
 function toggleDropDown() {
-if (!isDropDownOpen) {
-dropdownButton.innerHTML = `SELECT OPPONENT  &#8595`;
-isDropDownOpen = true;
-} else {
-dropdownButton.innerHTML = `SELECT OPPONENT  &#8592`;
-isDropDownOpen = false;
-}
-dropdownContent.classList.toggle('show');
+  if (!isDropDownOpen) {
+   dropdownButton.innerHTML = `SELECT OPPONENT  &#8595`;
+   isDropDownOpen = true;
+  } else {
+   dropdownButton.innerHTML = `SELECT OPPONENT  &#8592`;
+   isDropDownOpen = false;
+  }
+  dropdownContent.classList.toggle('show');
 }
